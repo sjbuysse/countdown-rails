29f1c8af817f14034988cb1bea02e9e988db48fb
Simon Buysse <buysse.simon@gmail.com>
2015-07-02 20:38:32 +0200

    added some methods to HappeningsController to get localtime from javascript

--- app/controllers/happenings_controller.rb
+++ app/controllers/happenings_controller.rb
@@ -1,5 +1,17 @@
 class HappeningsController < ApplicationController
     def index
+        setTimeZone
         @happenings = Happening.all
+    end
+
+    #This method calculated the difference in between the Happening date and the current date
+    #It will return a string that either says the difference in between those dates or "completed" 
+    def getTimeDifference(date2)
+
+    end
+
+    #Use Javascript on the front end to figure out the correct time to display, based on the user's browser
+    def setTimeZone
+        @localTime = params[:time]
     end
 end
