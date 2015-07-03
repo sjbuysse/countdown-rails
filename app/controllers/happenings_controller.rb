a3537179405467c8d786611b3b3e705d14600e0a
Simon Buysse <buysse.simon@gmail.com>
2015-07-03 11:31:07 +0200

    getTimeDifference action works perfect

--- app/controllers/happenings_controller.rb
+++ app/controllers/happenings_controller.rb
@@ -4,35 +4,35 @@ class HappeningsController < ApplicationController
         @happenings = Happening.all
     end
 
-    def test
-        puts "hi there"
-    end
-
-    #This method calculated the difference in between the Happening date and the current date
-    #It will return a string that either says the difference in between those dates or "completed" 
+    ##This method calculated the difference in between the Happening date and the current date
+    ##It will return a string that either says the difference in between those dates or "completed" 
     def getTimeDifference(now, date)
         differenceInSeconds = date.to_time - now.to_time
-        seconds = (differenceInSeconds % 60).floor
-        differenceInMinutes = differenceInSeconds/60
-        minutes = (differenceInMinutes % 60).floor
-        differenceInHours = differenceInMinutes/60
-        hours = (differenceInHours % 24).floor
-        days = (differenceInHours/24).floor
-        #countdownString = makeCountdownString(days, hours, minutes)
-        #return countdownString
+        if differenceInSeconds > 0
+            seconds = (differenceInSeconds % 60).floor
+            differenceInMinutes = differenceInSeconds/60
+            minutes = (differenceInMinutes % 60).floor
+            differenceInHours = differenceInMinutes/60
+            hours = (differenceInHours % 24).floor
+            days = (differenceInHours/24).floor
+            countdownString = makeCountdownString(days, hours, minutes)
+        else
+            countdownString = "completed"
+        end
+        return countdownString
     end
 
     def makeCountdownString(days, hours, minutes)
         if days > 0
-            return days + 'd, ' + hours + 'h, ' + minutes + 'm'
-        else if hours > 0
-            return  hours + 'h, ' + minutes + 'm'
-        else if minutes > 0
-            return minutes + ' min'
+            return "#{days} d, #{hours} h, #{minutes} m"
+        elsif hours > 0
+            return "#{hours} h, #{minutes} m"
+        elsif minutes > 0
+            return "#{minutes} m"
         end 
     end
 
-    #Use Javascript on the front end to figure out the correct time to display, based on the user's browser
+    ##Use Javascript on the front end to figure out the correct time to display, based on the user's browser
     def setTimeZone
         @localTime = params[:time]
     end
